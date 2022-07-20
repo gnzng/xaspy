09077060738ad21660a277e93fe3008b6a2af291
gnzng <65827185+gnzng@users.noreply.github.com>
2022-07-20 16:32:26 -0700

    Update _version.py

--- xaspy/_version.py
+++ xaspy/_version.py
@@ -2,5 +2,5 @@
 
 __name__    = 'xaspy'
 __date__    = '2022-07-20'
-__version__ = 'pre0.3.0'
+__version__ = 'pre0.3.0_1'
 __authors__ = "Damian Guenzing | Facaulty of Physics | University of Duisburg-Essen"
