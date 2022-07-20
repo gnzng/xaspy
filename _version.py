a33d5d09241753a550d00165940cab280f5cef1f
gnzng <65827185+gnzng@users.noreply.github.com>
2022-07-20 11:42:23 -0700

    updated

--- _version.py
+++ _version.py
@@ -1,6 +1,6 @@
 #! python
 
 __name__    = 'xaspy'
-__date__    = '2022-03-10'
-__version__ = '0.2.1'
+__date__    = '2022-07-20'
+__version__ = 'pre0.3.0'
 __authors__ = "Damian Guenzing | Facaulty of Physics | University of Duisburg-Essen"
