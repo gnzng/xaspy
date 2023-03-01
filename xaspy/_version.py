be67f95c546678dc5c7fae3f13224d98566a6998
gnzng <65827185+gnzng@users.noreply.github.com>
2023-03-01 07:35:08 +0100

    added helper function center() in utils module

--- xaspy/_version.py
+++ xaspy/_version.py
@@ -1,6 +1,6 @@
 #! python
 
 __name__ = "xaspy"
-__date__ = "2022-07-22"
-__version__ = "0.3.1"
+__date__ = "2023-03-01"
+__version__ = "0.3.2"
 __authors__ = "Damian Guenzing|damian.guenzing@uni-due.de\n\tAlpha T. N'Diaye|atndiaye@lbl.gov"
